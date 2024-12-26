70048c01753f3acb1a74d24af52c9ca803036fd1
saurabh-gadre <saurabhgadre94@gmail.com>
2024-12-26 22:08:23 +0530

    Playwright Reporting Config changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,8 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 4 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['allure-playwright', { outputFolder: 'playwright-report' }],['html',{ outputFolder: 'playwright-report' }],['list']],
+  //reporter: [['allure-playwright', { outputFolder: 'playwright-report' }],['html',{ outputFolder: 'playwright-report' }],['list']],
+  reporter: [['allure-playwright', { outputFolder: 'playwright-report' }],['list']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   globalSetup: require.resolve('./utils/global-setup.ts'),
   use: {
